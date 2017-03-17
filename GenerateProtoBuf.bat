2ba6cc60292affe8cc9ad556a251eedf734fc905
Quinn Damerell <qdamere@microsoft.com>
2017-03-17 11:41:48 -0700

    Renaming files, fixing up some commands.

--- GenerateProtoBuf.bat
+++ GenerateProtoBuf.bat
@@ -10,7 +10,7 @@ echo Ensuring protoc-gen-go is installed...
 go get -u github.com/golang/protobuf/protoc-gen-go
 
 echo Generating...
-protobuf-3.2.0\bin\protoc.exe -I="ftl\protocols" --cpp_out=ftl\photon-cpp --go_out=ftl\photon-go ftl\protocols\IngestConnection.proto
+protobuf-3.2.0\bin\protoc.exe -I="ftl\protocols" --cpp_out=ftl\photon-cpp --go_out=ftl\photon-go ftl\protocols\PhotonCommands.proto
 
 set goos = %goos%
 
